e13bd458f7ecca3e6581e280579ab40eb01ddb9e
DIBYANSHU JHA <85605269+dibyanshujha12345@users.noreply.github.com>
2022-04-03 16:48:04 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,8 @@
+number=input("Enter any Number")
+
+if(number<3):
+    print("U lose,enter a number greater than three")
+if(3<number<5):
+    print("U lose,enter a number greater than three")   
+if(number>7):
+    print("Congrats,u won")     
